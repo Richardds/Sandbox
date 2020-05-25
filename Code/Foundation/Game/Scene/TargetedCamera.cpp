aef4d47a395906563d091287cf0c41f219cb8c09
Richard Boldiš <richard@boldis.dev>
2020-05-25 10:07:20 +0200

    Fix targeted camera offsets

--- Code/Foundation/Game/Scene/TargetedCamera.cpp
+++ Code/Foundation/Game/Scene/TargetedCamera.cpp
@@ -19,11 +19,7 @@ void Graphics::TargetedCamera::Spectate(const Math::Vector3f& target)
     const float radiansX = glm::radians(this->_rotationX);
     const float radiansY = glm::radians(this->_rotationY);
 
-    const float offsetX = glm::sin(radiansY) * this->_distance;
-    const float offsetY = glm::sin(radiansX) * this->_distance;
-    const float offsetZ = glm::cos(radiansX) * this->_distance;
-
-    this->_position.x = target.x - offsetX;
-    this->_position.y = target.y + offsetY;
-    this->_position.z = target.z + offsetZ;
+    this->_position.x = target.x - this->_distance * glm::cos(radiansX) * glm::sin(radiansY);
+    this->_position.y = target.y + this->_distance * glm::sin(radiansX);
+    this->_position.z = target.z + this->_distance * glm::cos(radiansX) * glm::cos(radiansY);
 }
